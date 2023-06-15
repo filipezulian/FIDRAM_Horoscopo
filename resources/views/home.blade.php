bd697278558dd1483de2b2a34d611660e1764971
Arthur Cruz <espindola.art@gmail.com>
2023-06-14 21:47:23 -0300

    40% pronto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,9 +29,19 @@
             </nav>
         </div>
         <div class="info-mid-block">
-            <p>blocao</p>
+            <div class="ta-info-mid-block">
+                <textarea class="input-dados-dia" style="resize: none" readonly>{{$paragraphText}}</textarea>
+                <div>
+                    <img src="/img/home.svg" class="icone-casa" alt="casa">
+                    <textarea class="input-dados-domestico" style="resize: none" readonly>Pegar Dados Domestico</textarea>
+                </div>                
+                <div>
+                    <img src="/img/food.svg" class="icone-comida" alt="casa">
+                    <textarea class="input-dados-alimentar" style="resize: none" readonly>Pegar Dados Alimentar</textarea>
+                </div>                
+            </div>            
             <div class="share-line">
-                <p>linha</p>
+                <button class="btn-share-line"><img src="/img/share_icon.svg" alt=""></button>
             </div>
         </div>
         
